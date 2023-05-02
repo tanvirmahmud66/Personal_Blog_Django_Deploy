8fbea18c2605845c6fbf0964da96a533fca49118
Tanvir Mahmud <tanvirmahmud@Tanvirs-MacBook-Air.local>
2023-05-02 18:34:17 +0600

    static configuration

--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -7,12 +7,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]+static(settings.MEDIA_URL,
-         document_root=settings.MEDIA_ROOT)
+]
+
+# +static(settings.MEDIA_URL,
+#          document_root=settings.MEDIA_ROOT)
 
 
-print(static(settings.MEDIA_URL,
-             document_root=settings.MEDIA_ROOT))
+# print(static(settings.MEDIA_URL,
+#              document_root=settings.MEDIA_ROOT))
 
 
 urlpatterns += staticfiles_urlpatterns()
